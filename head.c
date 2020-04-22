6fadb02cc5a0a0e2b6ce2306586e54731474c1bc
Graeme Hosford <graeme.hosford@gmail.com>
2020-04-22 15:28:46 +0100

    Fixed some head.c probleems

--- head.c
+++ head.c
@@ -11,8 +11,8 @@ head(int fd, char *name) {
 
     l = c = 0;
 
-  while ((n = read(fd, buf, sizeof(buf))) > 0 ) {
-    for (i=0;i<=n ;i++) {				
+  while ((n = read(fd, buf, sizeof(buf))) > 0) {
+    for (i=0; i<=n; i++) {				
       if (buf[i]!='\n') {			
       	printf(1,"%c",buf[i]);
       }	else if (l == 9) {
@@ -26,7 +26,7 @@ head(int fd, char *name) {
   }
 
   if(n < 0) {
-    printf(1, "head: read error\n");
+    printf(1, "Error with reading in head.c\n");
     exit();
   }
 }
@@ -37,22 +37,26 @@ int
 main(int argc, char *argv[]) {
       int i;
       int fd = 0;
-      char *file;
+      char *file = "";
       char a;
-
-      file = "";
 
       if (argc <= 1) {	
             head(0, "");
             exit();
       } else {
-		    file = *argv[1];
+      for (i = 1; i < argc; i++) {
+		    a = *argv[i];
 		    
-			  if ((fd = open(file, 0)) < 0) {
-          printf(1, "head: cannot open %s\n", file);
+		    if (a == '-') {
+		      argv[i]++;
+		    } else {
+		    
+			  if ((fd = open(argv[i], 0)) < 0) {
+          printf(1, "Cannot open %s\n", argv[i]);
           exit();
         }
-		 
+		 }
+		 }
         head(fd, file);
         close(fd);
         exit();
